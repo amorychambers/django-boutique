5077a40252df089d2e8bae7797ad6219bdd2fd0f
Amory <108595086+amorychambers@users.noreply.github.com>
2024-08-16 19:54:56 +0100

    feat: add form handling and checkout_success to checkout/views.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,6 @@ from checkout import views
 
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
